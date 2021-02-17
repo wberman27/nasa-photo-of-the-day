467987e656174f03c715c6826be3f20b13e6b59f
Will B <wberman27@gmail.com>
2021-02-17 18:10:58 -0500

    building fundamentals

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from "react";
+import userEvent from "@testing-library/user-event";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import {BASE_URL, API_KEY} from './constants'
+import axios from 'axios'
+
+
+
+
+
 
 function App() {
+  
+
+  useEffect(() => {
+    const fetchData = () =>{
+      axios
+      .get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then(res =>{
+        console.log(res)
+      })
+      .catch(err =>{
+        console.log(err)
+      })
+    }
+    fetchData();
+
+  }, [])
+
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>
+        NASA Photo Of The Day <span role="img" aria-label='go!'>🚀</span>
+      </h1>
+
+
+
     </div>
   );
 }
